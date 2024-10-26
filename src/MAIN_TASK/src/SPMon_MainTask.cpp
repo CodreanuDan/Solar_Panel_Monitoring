baa9f9b105589fcc3f7bf84d398c69882400b62b
DanCodreanu2001 <dan.codreanu@student.tuiasi.ro>
2024-10-26 21:21:05 +0300

    Upload for the first demo code

---
+++ src/MAIN_TASK/src/SPMon_MainTask.cpp
@@ -0,0 +1,12 @@
+/******************************************************************************************************
+* Project name: Solar Panel Monitoring
+* Author: Codreanu Dan
+* File name: SPMon_MainTask.cpp
+* Descr: Contains the functions for SPMon_MainTask
+*
+*
+*
+*
+*
+* Version: 1.0, 26.10.2024
+******************************************************************************************************/
---
+++ src/SPMon.ino
@@ -0,0 +1,48 @@
+/******************************************************************************************************
+* Project name: Solar Panel Monitoring
+* Author: Codreanu Dan
+* File name: SPMon.ino
+* Descr: Main file of the project, contains the initalisation and setup for the components
+*
+*
+*
+*
+*
+* Version: 1.0, 19.10.2024
+******************************************************************************************************/
+
+/******************************************************************************************************
+* IMPORT AREA 
+******************************************************************************************************/
+#include "SPMon_DataTypes.h"
+#include "SPMon_InitTask.h"
+#include "SPMon_SensCalibTask.h"
+#include "SPMon_ComInitTask.h"
+
+/******************************************************************************************************
+* Function name: setup
+* Descr: Init. of the APP 
+* Params:
+*
+******************************************************************************************************/
+void setup() 
+{
+  /* To use serial monitor*/
+  Serial.begin(115200);
+  /* APP startup*/
+  spmonInit.SPMon_InitTask_CreateInitTask();
+}
+
+/******************************************************************************************************
+* Function name: loop
+* Descr: not used, due to the fact that we use tasks
+* Params:
+*
+******************************************************************************************************/
+void loop() 
+{
+  Serial.print(initFlag.SEN_CALIB_FLAG);
+  Serial.print(initFlag.COM_INIT_FLAG);
+  Serial.println("PLACEHOLDER");
+  delay(10000);
+}
